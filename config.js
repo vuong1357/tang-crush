848593a68e2c9cf44402fbfce2b9e4863726ebcb
vuong1357 <85486760+vuong1357@users.noreply.github.com>
2021-06-07 16:38:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Làm công chúa của anh từ giờ đến mãi về sau nha ❤.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/doanvuong1509' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
